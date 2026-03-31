0ba6d8ee244e948c4ec41fc81ae7e1c9f1896af7
israrsourcecode <israr.sourcecode@gmail.com>
2026-04-01 00:05:45 +0500

    "gallery done"

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,28 +1,81 @@
 import { useState } from 'react'
 import './Gallery.css'
+import ImgCard from '../Common/ImgCard';
 
 const Gallery = () => {
-    const [activeTab,setActiveTab]=useState("fiction")
-    // const handleTabe=
+    const booksData = {
+        fiction: [
+            { id: 1, img: "./src/assets/images/tab2.webp" },
+            { id: 2, img: "./src/assets/images/tab1.webp" },
+            { id: 3, img: "./src/assets/images/tab3.webp" },
+            { id: 4, img: "./src/assets/images/tab4.webp" },
+            { id: 5, img: "./src/assets/images/tab5.webp" },
+            { id: 6, img: "./src/assets/images/tab6.webp" },
+        ],
+        nonfiction: [
+            { id: 1, img: "./src/assets/images/tab4.webp" },
+            { id: 2, img: "./src/assets/images/tab5.webp" },
+            { id: 3, img: "./src/assets/images/tab2.webp" },
+            { id: 4, img: "./src/assets/images/tab3.webp" },
+            { id: 5, img: "./src/assets/images/tab6.webp" },
+            { id: 6, img: "./src/assets/images/tab1.webp" }
+        ],
+        biography: [
+            { id: 1, img: "./src/assets/images/tab3.webp" },
+            { id: 2, img: "./src/assets/images/tab6.webp" },
+            { id: 3, img: "./src/assets/images/tab1.webp" },
+            { id: 4, img: "./src/assets/images/tab2.webp" },
+            { id: 5, img: "./src/assets/images/tab4.webp" },
+            { id: 6, img: "./src/assets/images/tab5.webp" }
+        ],
+        childrenbooks: [
+            { id: 1, img: "./src/assets/images/tab2.webp" },
+            { id: 2, img: "./src/assets/images/tab3.webp" },
+            { id: 3, img: "./src/assets/images/tab1.webp" },
+            { id: 4, img: "./src/assets/images/tab4.webp" },
+            { id: 5, img: "./src/assets/images/tab5.webp" },
+            { id: 6, img: "./src/assets/images/tab6.webp" }
+        ],
+        autobiography: [
+            { id: 1, img: "./src/assets/images/tab4.webp" },
+            { id: 2, img: "./src/assets/images/tab1.webp" },
+            { id: 3, img: "./src/assets/images/tab3.webp" },
+            { id: 4, img: "./src/assets/images/tab5.webp" },
+            { id: 5, img: "./src/assets/images/tab6.webp" },
+            { id: 6, img: "./src/assets/images/tab2.webp" }
+        ],
+        businessbooks: [
+            { id: 1, img: "./src/assets/images/tab6.webp" },
+            { id: 2, img: "./src/assets/images/tab2.webp" },
+            { id: 3, img: "./src/assets/images/tab4.webp" },
+            { id: 4, img: "./src/assets/images/tab1.webp" },
+            { id: 5, img: "./src/assets/images/tab3.webp" },
+            { id: 6, img: "./src/assets/images/tab5.webp" }
+        ]
+    };
+    const [activeTab, setActiveTab] = useState("fiction")
+    const handleTabe = (tab) => {
+        setActiveTab(tab)
+    }
     return (
         <section className='GalleryTab'>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-3 col-lg-3">
+                    <div className="col-12 col-md-4 col-lg-4">
                         <div className="GalleryTabs">
-                            <button >Fiction (26)</button>
-                            <button>Non Fiction (8)</button>
-                            <button>Biography (19)</button>
-                            <button>Children Book (11)</button>
-                            <button>Autobiography (09)</button>
-                            <button>Business Books (08)</button>
+                            <button onClick={() => handleTabe("fiction")} className={activeTab === "fiction" ? "active" : ""}>Fiction (26)</button>
+                            <button onClick={() => handleTabe("nonfiction")} className={activeTab === "nonfiction" ? "active" : ""}>Non Fiction (8)</button>
+                            <button onClick={() => handleTabe("biography")} className={activeTab === "biography" ? "active" : ""}>Biography (19)</button>
+                            <button onClick={() => handleTabe("childrenbooks")} className={activeTab === "childrenbooks" ? "active" : ""}>Children Books (11)</button>
+                            <button onClick={() => handleTabe("autobiography")} className={activeTab === "autobiography" ? "active" : ""} >Autobiography (09)</button>
+                            <button onClick={() => handleTabe("businessbooks")} className={activeTab === "businessbooks" ? "active" : ""}>Business Books (08)</button>
                         </div>
                     </div>
-                    <div className="col-12 col-md-9 col-lg-9">
+                    <div className="col-12 col-md-8 col-lg-8">
                         <div className="gallery">
-                            <div className="card">
-                                <img className='img-fluid' src="./src/assets/images/tab1.webp" alt="" />
-                            </div>
+                            {booksData[activeTab].map((book) => (
+                                <ImgCard key={book.id} img={book.img} />
+                            ))}
                         </div>
                     </div>
                 </div>
